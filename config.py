93c40a359ba5615ce8ec654e38c1be11cab33232
Berdnikov K. <berdnikovk354@gmail.com>
2026-06-11 03:11:23 +0700

    мцывм

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 load_dotenv()
 
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
+PGHOST = os.environ.get("DB_HOST")
+PGPORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
+PGUSER = os.environ.get("DB_USER")
+PGPASSWORD = os.environ.get("DB_PASS")
